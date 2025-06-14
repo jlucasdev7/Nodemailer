60497cf11802d932b22bda7a113f62bdf577221d
Jlucas <jl159259510@gmail.com>
2025-06-14 15:47:47 -0300

    Criação do Nodemailer

---
+++ index.js
@@ -0,0 +1,22 @@
+const nodemailer = require('nodemailer');
+
+const transport = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, //true Para 465, false Para as Outras
+    auth: {
+        user: '', //Seu E-mail
+        pass: '',      //Sua Senha
+    }
+});
+
+transport.sendMail({
+    from: ' <>',  //'Nome <E-mail>'
+    to: '',                 //E-mail do Destinatário
+    subject: '', //Titulo Principal
+    html: '',   //Mensagem
+    text: '',  //Mensagem
+})
+
+.then(() => console.log('Email Enviado com Sucesso!'))
+.catch((err) => console.log('Erro ao Enviar o Email: ',err));
